5432d82b263376d38abc96dc5b493c27063db590
Yusuf Abdulrahman <yusufabdulrahman05@gmail.com>
2022-04-12 11:47:34 +0100

    update

--- matsfar/login/views.py
+++ matsfar/login/views.py
@@ -50,67 +50,11 @@ def geometric(request):
     else:
         l = float(l)
         w = float(w)       
-        c = 'The area of a rectangle ' + str(l*w) +'cm^2'
+        area = 'The area of the rectangle = ' + str(l*w) +'cm^2'
+        peri = 'The perimeter of the rectangle = ' + str((2*l)+(2*w)) +'cm'
         context = {
-            'res':c 
-    
-            }
-            
-        return render(request, 'geometric.html', context)
-
-
-def geometric(request):
-
-    l = request.POST['length']
-    w = request.POST['width']
-
-    if l == '' or w == '':
-        context = {'res':'No Data Entered!!!'}
-        return render(request, 'geometric.html', context)  
-
-    else:
-        l = float(l)
-        w = float(w)       
-        d = 'The perimeter of a rectangle' + str(2*l + 2*w) + 'cm^2'
-        context = {
-            'res':d
-    
-            }
-            
-        return render(request, 'geometric.html', context)
-
-
-def geometric(request):
-
-    l = request.POST['length']
-    
-    if l == '' :
-        context = {'res':'No Data Entered!!!'}
-        return render(request, 'geometric.html', context)  
-
-    else:
-        l = float(l)       
-        d = 'The length of a square' + str(l * l) + 'cm^2'
-        context = {
-            'res':d
-    
-            }
-            
-        return render(request, 'geometric.html', context)
-
-def geometric(request):
-
-    l = request.POST['length']
-    
-    if l == '' :
-        context = {'res':'No Data Entered!!!'}
-        return render(request, 'geometric.html', context)  
-
-    else:
-        l = float(l)       
-        d = 'The length of a square' + str(4 * l) + 'cm^2'
-        context = {
-            'res':d
+            'area':area, 
+            'peri':peri
     
             }
             
