aa072c4c06f9189f30e6f9998350150ace5873af
faridat <bellofaridat1@gmail.com>
2022-04-14 12:46:01 +0100

    update

--- matsfar/login/urls.py
+++ matsfar/login/urls.py
@@ -5,8 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('sum',views.sum, name='sum'),
-     path('rectangle',views.rectangle, name='rectangle'),
-     path('square',views.square, name='square'),
-     path('circle',views.circle, name='circle'),
-     path('index', views.index, name='index'),
+    path('rectangle',views.rectangle, name='rectangle'),
+     
 ]
