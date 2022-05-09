4497197a5d7e4f38798413ce41a00796bd33d44a
faridat <bellofaridat1@gmail.com>
2022-05-09 09:51:15 +0100

    update

--- matsfar/login/models.py
+++ matsfar/login/models.py
@@ -1,9 +1,11 @@
+import email
 from django.db import models
 
 # Create your models here.
 
-class intel:
-    id = int
-    det = str
-    st = bool
+class intel(models.Model):
+    name =models.CharField(max_length=100)
+    Det =models.CharField(max_length=255)
+    email ==models.CharField(max_length=255)
+
 
--- matsfar/login/urls.py
+++ matsfar/login/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('sum',views.sum, name='sum'),
-    path('rectangle',views.rectangle, name='rectangle'),
+    path('signin',views.signin, name='signin'),
+    
      
 ]
